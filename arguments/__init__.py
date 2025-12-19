6ed5c057d488247dd3eb8ad2341772ee343650eb
Landon Dyken <ldyke@uic.edu>
2025-12-19 11:51:13 -0700

    Change learning rate parameters

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -43,7 +43,7 @@ class ModelParams(ParamGroup):
         self._model_path = ""
         self._images = "images"
         self._resolution = -1
-        self._white_background = False # default: False
+        self._white_background = True # default: False
         self.data_device = "cuda"
         self.eval = False
         self.debug_cuda = False
@@ -84,8 +84,8 @@ class OptimizationParams(ParamGroup):
         self.position_lr_max_steps = 30_000
         self.normal_lr = 0.01
         self.sh_lr = 0.0025
-        self.opacity_lr = 0.05 #TODO: lower this
-        self.scalar_lr = 0.0125
+        self.opacity_lr = 0.025
+        self.scalar_lr = 0.00025
         self.scaling_lr = 0.005
         self.rotation_lr = 0.001
         # self.scaling_lr = 0.001
